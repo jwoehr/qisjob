5b9ce27c7751c0279652e4f52db2fe4b390b9983
Jack J. Woehr <jwoehr@softwoehr.com>
2019-02-09 16:47:29 -0700

    Restructure objects

--- ibmqe_job/ibmqe_jobmgr.py
+++ ibmqe_job/ibmqe_jobmgr.py
@@ -7,34 +7,72 @@
 
 from IBMQuantumExperience import IBMQuantumExperience
 
+
+class IBMQEJobSpec:
+    """
+    Job specification for IBMQEJobManager to run and manage
+    """
+
+    def __init__(self, qasm=None filepath=None):
+        self.qasm = qasm
+        if filepath is not None:
+            self.load_qasm(filepath)
+
+    def load_qasm(self, filepath):
+        fh = open(filepath, 'r')
+        self.qasm = fh.read()
+        fh.close()
+
+    def job_spec(self):
+        return {'qasm': self.qasm}
+
+
 class IBMQEJobMgr:
-	"""Make it easier to extract info from the job dictionary
-	created by IBMQuantumExperience instance"""
+    """
+    Manage jobs based on an IBMQuantumExperience.IBMQuantumExperience instance
+    """
 
-	def __init__(self, jobdict):
-		self.jobdict = jobdict
+    def __init__(self, ibmqe, backend='ibmq_qasm_simulator',
+                 counts=1024, credits=3):
+        """
+        ibmqe	...	the IBMQuantumExperience instance
+        backend	...	name of backend
+        counts	...	times to run
+        credits	...	max credits to expend
+        """
+        self.ibmqe = ibmqe
+        self.backend = backend
+        self.counts = counts
+        self.credits = credits
+        self.run_dict = {}  # dict returned by run_job()
+        self.jobs_dict = {}  # jobs dict from run_job()
+        self.job_list = []  # list of jobs to run
+        self.jobs = []		# list of jobs from jobs_dict
 
-	def get_id(self):
-		return self.jobdict['executionId']
+    def parse_jobs(self):
+        """Extract list of jobs from dictionary hierarchy"""
+        self.jobs = []
+        self.jobs_dict = self.run_dict['qasms']
+        for i in jobs_dict:
+            self.jobs.append(i)
 
-	def get_status(self):
-		return self.jobdict['status']
+    def add_job(self, jobspec):
+        """Add a job to run"""
+        self.job_list.append(jobspec.job_spec())
 
-class IBMQEJobsMgr:
-	"""Manage the array of jobs which is returned by
-	IBMQuantumExperience.IBMQuantumExperience.run_job()"""
+    def run_jobs(self):
+        """Pass job list and other parameters to the backend"""
+        self.run_dict = self.ibmqe.run_job(
+            self.job_list, self.backend, self.counts, self, credits)
 
-	def __init__(self, ibmqe, jobs):
-		self.ibmqe = ibmqe
-		self.jobs = jobs
-		self.job_mgr_list = self.parse_jobs()
+    def get_jobnum(self, index):
+        """Get job number at server from job record at index in jobs"""
+        return jobs[index]['executionId']
 
-	def parse_jobs(self):
-		l = []
-		jobs_dict = self.jobs['qasms'];
-		for i in jobs_dict:
-			l.append(IBMQEJobMgr(i))
-		return l
+    def get_status(self, jobnum):
+        """Get job status at server from from server job number"""
+        return self.ibmqe.get_job(jobnum)
 
-	# def get_backend(self):
-		# return self.jobdict['backend']
+    def get_job_status(self, index):
+        """Get job status at server from index in list of jobs running"""
+        return self.get_status(self.get_jobnum(index))
