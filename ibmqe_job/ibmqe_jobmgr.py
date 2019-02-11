7ffa221894c51eec9a01e7784eb268c6f8d4693c
Jack J. Woehr <jwoehr@softwoehr.com>
2019-02-11 12:35:00 -0700

    Now has a good __main__

--- ibmqe_job/ibmqe_jobmgr.py
+++ ibmqe_job/ibmqe_jobmgr.py
@@ -1,12 +1,14 @@
-# ibmqe_jobmgr.py
-# Class to manage jobs loading qasm source and run jobs with reporting in CSV
-# Copyright 2019 Jack Woehr jwoehr@softwoehr.com PO Box 51, Golden, CO 80402-0051
-# BSD-3 license -- See LICENSE which you should have received with this code.
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
+"""
+ibmqe_jobmgr.py
+Class to manage jobs loading qasm source and run jobs with reporting in CSV
+Copyright 2019 Jack Woehr jwoehr@softwoehr.com PO Box 51, Golden, CO 80402-0051
+BSD-3 license -- See LICENSE which you should have received with this code.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
 
-# https://github.com/Qiskit/qiskit-api-py
-# pip install qiskit-api-py
+https://github.com/Qiskit/qiskit-api-py
+pip install qiskit-api-py
+"""
 from IBMQuantumExperience import IBMQuantumExperience
 
 
@@ -428,18 +430,98 @@ class IBMQEJobMgr:
 
 
 if __name__ == "__main__":
+
     explanation = """ibmqe_jobmgr.py :
 Class to manage jobs loading qasm source and run jobs with reporting in CSV
 Copyright 2019 Jack Woehr jwoehr@softwoehr.com PO Box 51, Golden, CO 80402-0051
 BSD-3 license -- See LICENSE which you should have received with this code.
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
+Exits (1) on status error.
+Exits (2) on device error.
+Exits (200) on no backend chosen.
+Exits (300) on no filepaths given.
 """
     import argparse
+    import sys
+    import time
+    import datetime
+
+    now = datetime.datetime.now().isoformat()
+
     parser = argparse.ArgumentParser(description=explanation)
-    parser.add_argument(
-        "token", help="""IBM Q Experience API token
-        https://quantumexperience.ng.bluemix.net/qx/account/advanced""")
-    parser.add_argument("filepath", help="Filepath to .qasm file")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-s", "--sim", action="store_true",
+                       help="Use IBMQ qasm simulator")
+    group.add_argument("-b", "--backend", action="store",
+                       help="Use specified IBM backend")
+    parser.add_argument("-c", "--credits", type=int, action="store", default=3,
+                        help="Max credits to expend on run, default is 3")
+    parser.add_argument("-n", "--name", action="store",
+                        default=now,
+                        help="Your name for this experiment, default is timestamp")
+    parser.add_argument("-m", "--timeout", type=int, action="store", default=60,
+                        help="Timeout in seconds, default is 60")
+    parser.add_argument("-o", "--outfile", action="store",
+                        help="Write CSV to outfile overwriting silently, default is stdout")
+    parser.add_argument("-q", "--qubits", type=int, action="store", default=5,
+                        help="Number of qubits for the experiment, default is 5")
+    parser.add_argument("-t", "--shots", type=int, action="store", default=1024,
+                        help="Number of shots for the experiment, default is 1024")
+    parser.add_argument("-u", "--url",  action="store", default='https://quantumexperience.ng.bluemix.net/api',
+                        help="URL, default is https://quantumexperience.ng.bluemix.net/api")
+    parser.add_argument("-v", "--verbose", action="count", default=0,
+                        help="Increase verbosity each -v up to 3")
+    parser.add_argument("token", help="""IBM Q Experience API token
+                    (See https://quantumexperience.ng.bluemix.net/qx/account/advanced)""")
+    parser.add_argument("filepaths", nargs=argparse.REMAINDER,
+                        help="Filepaths to 1 or more .qasm files")
+
     args = parser.parse_args()
-    IBMQEJobMgr.test(args.token, args.filepath)
+
+    # print(args)
+
+    def verbosity(text, count):
+        if args.verbose >= count:
+            print(text)
+
+    # Connect to IBMQE
+    api = IBMQuantumExperience(args.token, config={
+        "url": args.url}, verify=True)
+
+    # Choose backend
+    if args.backend:
+        backend = args.backend
+    elif args.sim:
+        backend = 'ibmq_qasm_simulator'
+    else:
+        print("No backend chosen, exiting.")
+        exit(200)
+
+    # Check for filepath(s)
+    if args.filepaths == []:
+        print("No filepath(s) for qasm file(s) given, exiting.")
+        exit(300)
+
+    jx = IBMQEJobMgr(api, backend=backend,
+                     counts=args.shots, credits=args.credits)
+
+    for filepath in args.filepaths:
+        jx.add_exec(IBMQEJobSpec(filepath=filepath))
+
+    jx.run_job()
+
+    while jx.get_job_status() != 'COMPLETED':
+        print(jx.get_job_status())
+        time.sleep(4)
+
+    print(jx.get_job())
+    
+    for i in range(0, len(jx.execs)):
+        csv = jx.csv_execution(args.filepaths[i], i)
+        for c in csv:
+            print(c)
+
+    exit()
+
+# Fin
