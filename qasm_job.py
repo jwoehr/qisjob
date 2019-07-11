09e3b016de2c28e91a451783e70ce39fa9fc6013
Jack J. Woehr <jwoehr@softwoehr.com>
2019-07-11 15:12:40 -0600

    works with job loop but credentials reissued

--- qasm_job.py
+++ qasm_job.py
@@ -58,7 +58,7 @@ parser.add_argument("--qiskit_version", action="store_true",
 parser.add_argument("-r", "--result", action="store_true",
                     help="Print job result")
 parser.add_argument("-t", "--shots", type=int, action="store", default=1024,
-                    help="Number of shots for the experiment, default is 1024")
+                    help="Number of shots for the experiment, default 1024, max 8192")
 parser.add_argument("-v", "--verbose", action="count", default=0,
                     help="Increase verbosity each -v up to 3")
 parser.add_argument("-x", "--transpile", action="store_true",
@@ -67,14 +67,22 @@ parser.add_argument("--token", action="store",
                     help="Use this token if a --url argument is also provided")
 parser.add_argument("--url", action="store",
                     help="Use this url if a --token argument is also provided")
-parser.add_argument("filepath", nargs='?',
-                    help="Filepath to .qasm file, default is stdin")
+parser.add_argument("filepaths", nargs='*',
+                    help="Filepath(s) to .qasm file, default is stdin")
 
 args = parser.parse_args()
 
 if (args.token and not args.url) or (args.url and not args.token):
     print('--token and --url must be used together or not at all', file=sys.stderr)
     exit(1)
+
+# Utility functions
+# #################
+
+def verbosity(text, count):
+    """Print text if count exceeds verbose level"""
+    if args.verbose >= count:
+        print(text)
 
 
 def account_fu(token, url):
@@ -85,6 +93,134 @@ def account_fu(token, url):
         p = IBMQ.load_account()
     return p
 
+
+def csv_str(description, sort_keys, sort_counts):
+    """Generate a cvs as a string from sorted keys and sorted counts"""
+    csv = []
+    csv.append(description)
+    keys = ""
+    for key in sort_keys:
+        keys += key + ';'
+    csv.append(keys)
+    counts = ""
+    for count in sort_counts:
+        counts += str(count) + ';'
+    csv.append(counts)
+    return csv
+
+# Do job loop
+# ###########
+
+def one_job(filepath, outfile,
+    aer, token, url, b_end,
+    sim, qb, xpile,
+    shots, memory, j_b, res):
+    """Load qasm and run the job, print csv and other selected output"""
+
+    if filepath is None:
+        filepath = sys.stdin
+    if outfile is None:
+        outfile = sys.stdout
+
+    # Get file
+    verbosity("File path is " + ("stdin" if filepath is sys.stdin else filepath), 2)
+    ifh = filepath if filepath is sys.stdin else open(filepath, "r")
+    verbosity("File handle is " + str(ifh), 3)
+
+    # Read source
+    qasm_source = ifh.read()
+    ifh.close()
+    verbosity("qasm source:\n" + qasm_source, 1)
+
+    # Create circuit
+    circ = QuantumCircuit.from_qasm_str(qasm_source)
+    verbosity(circ.draw(), 2)
+
+    # Choose backend
+    backend = None
+    if args.aer:
+        # Import Aer
+        from qiskit import BasicAer
+        # Run the quantum circuit on a statevector simulator backend
+        backend = BasicAer.get_backend('statevector_simulator')
+    else:
+        provider = account_fu(token, url)
+        if b_end:
+            backend = provider.get_backend(b_end)
+        elif sim:
+            backend = provider.get_backend('ibmq_qasm_simulator')
+        else:
+            from qiskit.providers.ibmq import least_busy
+            large_enough_devices = provider.backends(
+                filters=lambda x: x.configuration().n_qubits >= args.qubits
+                and not x.configuration().simulator)
+            backend = least_busy(large_enough_devices)
+        verbosity("The best backend is " + backend.name(), 2)
+
+    verbosity("Backend is " + str(backend), 1)
+
+    if backend is None:
+        print("No backend available, quitting.")
+        exit(100)
+
+    # Transpile if requested and available and show transpiled circuit
+    # ################################################################
+
+    if xpile:
+        try:
+            print(transpile(circ, backend=backend))
+        except NameError:
+            print('Transpiler not available this Qiskit level.', file=sys.stderr)
+
+    # Prepare job
+    # ###########
+
+    # Maximum number of credits to spend on executions.
+    max_credits = args.credits
+
+    # Execute
+    job_exp = execute(circ, backend=backend, shots=shots,
+                  max_credits=max_credits, memory=memory)
+    if j_b:
+        print(job_exp)
+
+    job_monitor(job_exp)
+    result_exp = job_exp.result()
+
+    if res:
+        print(result_exp)
+
+    counts_exp = result_exp.get_counts(circ)
+    verbosity(counts_exp, 1)
+    sorted_keys = sorted(counts_exp.keys())
+    sorted_counts = []
+    for i in sorted_keys:
+        sorted_counts.append(counts_exp.get(i))
+
+    # Raw data if requested
+    if memory:
+        print(result_exp.data())
+
+    # Generate CSV
+    output = csv_str(str(backend) + ' ' + datetime.datetime.now().isoformat(),
+                     sorted_keys, sorted_counts)
+
+    # Open outfile
+    verbosity("Outfile is " + ("stdout" if outfile is sys.stdout else outfile), 2)
+    ofh = outfile if outfile is sys.stdout else open(outfile, "w")
+    verbosity("File handle is " + str(ofh), 3)
+
+    # Write CSV
+    for line in output:
+        ofh.write(line + '\n')
+    if outfile is not sys.stdout:
+        ofh.close()
+
+# Main
+# ####
+
+# Informational queries
+# #####################
 
 if args.properties:
     provider = account_fu(args.token, args.url)
@@ -103,141 +239,19 @@ if args.qiskit_version:
     pp.pprint(__qiskit_version__)
     exit(0)
 
-
-def verbosity(text, count):
-    """Print text if count exceeds verbose level"""
-    if args.verbose >= count:
-        print(text)
-
-
-# Choose filepath
-filepath = None
-if args.filepath is None:
-    filepath = sys.stdin
+if not args.filepaths:
+    one_job(None, args.outfile,
+        args.aer, args.token, args.url,
+        args.backend, args.sim, args.qubits,
+        args.transpile, args.shots,
+        args.memory, args.job, args.result)
 else:
-    filepath = args.filepath
-
-# Choose outfile
-outfile = None
-if args.outfile is None:
-    outfile = sys.stdout
-else:
-    outfile = args.outfile
-
-verbosity("File path is " + ("stdin" if filepath is sys.stdin else filepath), 2)
-
-ifh = filepath if filepath is sys.stdin else open(filepath, "r")
-
-verbosity("File handle is " + str(ifh), 3)
-
-# Read source
-qasm_source = ifh.read()
-ifh.close()
-verbosity("qasm source:\n" + qasm_source, 1)
-
-# Create circuit
-circ = QuantumCircuit.from_qasm_str(qasm_source)
-verbosity(circ.draw(), 2)
-
-# Choose backend
-backend = None
-if args.aer:
-    # Import Aer
-    from qiskit import BasicAer
-    # Run the quantum circuit on a statevector simulator backend
-    backend = BasicAer.get_backend('statevector_simulator')
-else:
-    provider = account_fu(args.token, args.url)
-    if args.backend:
-        backend = provider.get_backend(args.backend)
-    elif args.sim:
-        backend = provider.get_backend('ibmq_qasm_simulator')
-    else:
-        from qiskit.providers.ibmq import least_busy
-        large_enough_devices = provider.backends(
-            filters=lambda x: x.configuration().n_qubits >= args.qubits
-            and not x.configuration().simulator)
-        backend = least_busy(large_enough_devices)
-        verbosity("The best backend is " + backend.name(), 2)
-
-verbosity("Backend is " + str(backend), 1)
-
-if backend is None:
-    print("No backend available, quitting.")
-    exit(100)
-
-# Transpile if requested and available and show transpiled circuit
-# ################################################################
-
-try:
-    transpile
-except NameError:
-    args.transpile = None
-
-if args.transpile:
-    print(transpile(circ, backend=backend))
-
-# Prepare job
-# ###########
-
-# Number of shots to run the program (experiment); maximum is 8192 shots.
-shots = args.shots
-# Maximum number of credits to spend on executions.
-max_credits = args.credits
-
-
-def csv_str(description, sort_keys, sort_counts):
-    """Generate a cvs as a string from sorted keys and sorted counts"""
-    csv = []
-    csv.append(description)
-    keys = ""
-    for key in sort_keys:
-        keys += key + ';'
-    csv.append(keys)
-    counts = ""
-    for count in sort_counts:
-        counts += str(count) + ';'
-    csv.append(counts)
-    return csv
-
-
-# Execute
-job_exp = execute(circ, backend=backend, shots=shots,
-                  max_credits=max_credits, memory=args.memory)
-if args.job:
-    print(job_exp)
-
-job_monitor(job_exp)
-result_exp = job_exp.result()
-
-if args.result:
-    print(result_exp)
-
-counts_exp = result_exp.get_counts(circ)
-verbosity(counts_exp, 1)
-sorted_keys = sorted(counts_exp.keys())
-sorted_counts = []
-for i in sorted_keys:
-    sorted_counts.append(counts_exp.get(i))
-
-# Raw data if requested
-if args.memory:
-    print(result_exp.data())
-
-# Generate CSV
-output = csv_str(str(backend) + ' ' + datetime.datetime.now().isoformat(),
-                 sorted_keys, sorted_counts)
-
-# Open outfile
-verbosity("Outfile is " + ("stdout" if outfile is sys.stdout else outfile), 2)
-ofh = outfile if outfile is sys.stdout else open(outfile, "w")
-verbosity("File handle is " + str(ofh), 3)
-
-# Write CSV
-for line in output:
-    ofh.write(line + '\n')
-if outfile is not sys.stdout:
-    ofh.close()
+    for filepath in args.filepaths:
+        one_job(filepath, args.outfile,
+            args.aer, args.token, args.url,
+            args.backend, args.sim, args.qubits,
+            args.transpile, args.shots,
+            args.memory, args.job, args.result)
 
 verbosity('Done!', 1)
 
