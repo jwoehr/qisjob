9fe4f3b489bd17a8e2565f57362d5dc2fa68af61
Jack J. Woehr <jwoehr@softwoehr.com>
2019-12-25 14:30:10 -0700

    Moving towards module

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 25 12:04:26 2019
+
+@author: jax
+"""
+from setuptools import setup  # , find_packages
+setup(
+    name="qis_job",
+    version="3.0",
+    description="Run Qiskit jobs from command line",
+    url="hhtps://github.com/jwoehr/qis_job",
+    author="Jack Woehr",
+    author_email="jwoehr@softwoehr.com",
+    license="Apache 2.0",
+    packages=['qis_job'],
+    include_package_data=True,
+    package_data={ 'qis_job' : ['share/*examples'] },
+    scripts=['scripts/qisjob'],
+    zip_safe=False
+)
+
