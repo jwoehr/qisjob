4638ffc6eee4eed4110f57d8579e197eb3639216
Jack J. Woehr <jwoehr@softwoehr.com>
2019-12-30 13:40:53 -0700

    cythonized pprint state vector pprint memory

--- setup.py
+++ setup.py
@@ -5,22 +5,26 @@ Created on Wed Dec 25 12:04:26 2019
 
 @author: jax
 """
-from setuptools import setup # , find_packages
+from setuptools import setup  # , find_packages
 from distutils.dir_util import copy_tree
+from Cython.Build import cythonize
+
 setup(
     name="qis_job",
     version="3.0",
     description="Run Qiskit jobs from command line",
-    url="hhtps://github.com/jwoehr/qis_job",
+    url="https://github.com/jwoehr/qis_job",
     author="Jack Woehr",
     author_email="jwoehr@softwoehr.com",
     license="Apache 2.0",
     packages=['qis_job'],
     include_package_data=True,
-    package_data={ 'qis_job' : ['share/*examples'] },
+    package_data={'qis_job': ['share/*examples']},
     scripts=['scripts/qisjob'],
-    zip_safe=False
+    zip_safe=False,
+    ext_modules=cythonize("qis_job/qis_job.pyx",
+                          compiler_directives={'language_level': "3"})
 )
 
-copy_tree('share/qasm_examples', '/share/qis_job')
-copy_tree('share/qc_examples', '/share/qis_job')
+# copy_tree('share/qasm_examples', '/share/qis_job')
+# copy_tree('share/qc_examples', '/share/qis_job')
