8b497f15ffe1a7164ea92685004b99d026205f3a
jwoehr <jwoehr@softwoehr.com>
2022-11-01 09:35:58 -0600

    more work on the Tk interface

--- qisjob/qisjobtk.py
+++ qisjob/qisjobtk.py
@@ -2,11 +2,22 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Oct 31 19:51:37 2022
+Tk interface for QisJob
+The main repository is https://github.com/jwoehr/qisjob
+
+Copyright 2022 Jack Woehr jwoehr@softwoehr.com PO Box 82, Beulah, CO 81023-0082
+Apache License, Version 2.0 -- See LICENSE which you should have received with this code.
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
 @author: jwoehr
 """
 
-from tkinter import *
+from tkinter import Tk, Text, N, W, E, S
 from tkinter import ttk
 from qisjob import QisJob
 
