7ed7fb75834a35e43c050ff02e3bb98b477418f8
jwoehr <jwoehr@softwoehr.com>
2022-10-31 21:09:46 -0600

    Comments and a little more work

--- qisjob/qisjobtk.py
+++ qisjob/qisjobtk.py
@@ -6,25 +6,49 @@ Created on Mon Oct 31 19:51:37 2022
 @author: jwoehr
 """
 
-import tkinter
+from tkinter import *
 from tkinter import ttk
 from qisjob import QisJob
 
+
 class QisJobTk:
-    def __init__(self, qisjob: QisJob ):
+    """ """
+
+    def __init__(self, qisjob: QisJob):
+        """
+
+
+        Parameters
+        ----------
+        qisjob : QisJob
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
         self.qisjob = qisjob
-        self.root = tkinter.Tk()
+        self.root = Tk()
         self.frame = ttk.Frame(self.root)
         self.notebook = ttk.Notebook(self.frame)
         self.tab_printself = ttk.Frame(self.notebook)
         self.notebook.add(self.tab_printself, text="Self")
-        self.text_printself = tkinter.Text(self.tab_printself)
-        self.text_printself.insert('end', str(self.qisjob))
+        self.text_printself = Text(self.tab_printself)
+        self.text_printself.insert("end", str(self.qisjob))
+
     def run(self):
+        """
+
+
+        Returns
+        -------
+        None.
+
+        """
         self.root.grid()
-        self.frame.grid()
-        self.notebook.grid()
-        self.tab_printself.grid()
-        self.text_printself.grid()
+        self.frame.grid(sticky=(N, W, E, S))
+        self.notebook.grid(sticky=(N, W, E, S))
+        self.tab_printself.grid(sticky=(N, W, E, S))
+        self.text_printself.grid(sticky=(N, W, E, S))
         self.root.mainloop()
-    
