e901442406193e27c0c2b36085650b02168f1c65
jwoehr <jwoehr@softwoehr.com>
2022-12-24 11:48:24 -0700

    uncythonize setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,7 @@ Created on Wed Dec 25 12:04:26 2019
 
 @author: jax
 """
-import sys
-from setuptools import setup  # , find_packages
-try:
-    from Cython.Build import cythonize
-except ImportError:
-    import subprocess
-    subprocess.call([sys.executable, '-m', 'pip', 'install', 'Cython>=0.27.1'])
-    from Cython.Build import cythonize
+from setuptools import setup
 
 setup(
     name="qisjob",
@@ -22,26 +15,31 @@ setup(
     author="Jack Woehr",
     author_email="jwoehr@softwoehr.com",
     license="Apache 2.0",
-    packages=['qisjob'],
-    data_files=[('share/qisjob/qasm_examples',
-                 ['share/qasm_examples/ghzstate3q.qasm',
-                  'share/qasm_examples/ghzxxx.qasm',
-                  'share/qasm_examples/ghzxyx.qasm',
-                  'share/qasm_examples/ghzxyy.qasm',
-                  'share/qasm_examples/ghzyxy.qasm',
-                  'share/qasm_examples/ghzyyx.qasm',
-                  'share/qasm_examples/mermin_inequality_a.qasm',
-                  'share/qasm_examples/mermin_inequality_b.qasm',
-                  'share/qasm_examples/mermin_inequality_c.qasm',
-                  'share/qasm_examples/mermin_inequality_d.qasm',
-                  'share/qasm_examples/multimeasure.qasm',
-                  'share/qasm_examples/onebit.qasm',
-                  'share/qasm_examples/yiqing.qasm',
-                  'share/qasm_examples/yiqing_simple.qasm']),
-                ('share/qisjob/qc_examples',
-                 ['share/qc_examples/google_quantum_supremacy.py'])],
-    scripts=['scripts/qisjob'],
+    packages=["qisjob"],
+    data_files=[
+        (
+            "share/qisjob/qasm_examples",
+            [
+                "share/qasm_examples/entangle.qasm",
+                "share/qasm_examples/ghzstate3q.qasm",
+                "share/qasm_examples/ghzxxx.qasm",
+                "share/qasm_examples/ghzxyx.qasm",
+                "share/qasm_examples/ghzxyy.qasm",
+                "share/qasm_examples/ghzyxy.qasm",
+                "share/qasm_examples/ghzyyx.qasm",
+                "share/qasm_examples/mermin_inequality_a.qasm",
+                "share/qasm_examples/mermin_inequality_b.qasm",
+                "share/qasm_examples/mermin_inequality_c.qasm",
+                "share/qasm_examples/mermin_inequality_d.qasm",
+                "share/qasm_examples/multimeasure.qasm",
+                "share/qasm_examples/onebit.qasm",
+                "share/qasm_examples/yiqing.qasm",
+                "share/qasm_examples/yiqing_simple.qasm",
+            ],
+        ),
+        ("share/qisjob/qc_examples", ["share/qc_examples/google_quantum_supremacy.py"]),
+    ],
+    scripts=["scripts/qisjob"],
     zip_safe=False,
-    ext_modules=cythonize("qisjob/qisjob.pyx",
-                          compiler_directives={'language_level': "3"})
+    modules=["qisjob/qisjob"],
 )
