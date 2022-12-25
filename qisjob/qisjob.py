a27db4883e532076739de06b2dd444f39a2fca44
jwoehr <jwoehr@softwoehr.com>
2022-12-24 22:43:21 -0700

    added _str_ method already used in tk branch

--- qisjob/qisjob.py
+++ qisjob/qisjob.py
@@ -46,7 +46,7 @@ here to make it easier to understand what QisJob does in its `do_it()` method.
 
 import argparse
 import datetime
-from io import TextIOWrapper
+from io import StringIO, TextIOWrapper
 import pprint
 import sys
 from typing import Any
@@ -742,6 +742,11 @@ class QisJob:  # pylint: disable-msg=too-many-instance-attributes, too-many-publ
         self.job_monitor_filepath = job_monitor_filepath
         self.job_monitor_line = job_monitor_line
         self.noisy_sim = noisy_sim
+
+    def __str__(self) -> str:
+        out = StringIO()
+        pprint.pprint(self.__dict__, out)
+        return "{}\n{}".format(super().__str__(), out.getvalue())
 
     def qisjob_version(self) -> str:
         """
