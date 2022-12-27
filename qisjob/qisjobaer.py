b98ab6c6ecc9128a9aa6f30340dd498ac5744e67
jwoehr <jwoehr@softwoehr.com>
2022-12-27 10:54:06 -0700

    more on aer simulator manager

--- qisjob/qisjobaer.py
+++ qisjob/qisjobaer.py
@@ -32,20 +32,81 @@ class QisJobAer:
     Class to manage the Qiskit AerSimulator for QisJob
     """
 
+    simulator_kwargs = [
+        "backend_named",
+        "configuration",
+        "method",
+        "noise_model",
+        "noise_model_backend",
+        "properties",
+    ]
+
     def __init__(self, **kwargs):
+        """
+        Instance this Aer Simulator manager
+
+        Parameters
+        ----------
+        **kwargs :
+            Arguments passed in from QisJob to be processed into valid
+            arguments for instancing an Aer Simulator.
+
+        Returns
+        -------
+        None.
+
+        """
         self.configuration = None
         self.properties = None
         self.provider = None
         self.method = None
         self.backend = None
-        self.from_backend_named = None
+        self.backend_named = None
         self.process_kwargs(**kwargs)
-        self.aer_simulator = AerSimulator()
+        self.aer_simulator = self.instance_aer_simulator()
 
     def process_kwargs(self, kwargs):
+        """
+        Process string kwargs from QisJob to formulate valid arguments to
+        instance an AerSimulator.
+
+        Parameters
+        ----------
+        kwargs : dict
+            kwargs as passed in from QisJob to be translated to valid args/kwargs
+            to instance an AerSimulator.
+
+        Raises
+        ------
+        QisJobArgumentException
+            If any kwargs aren't recognized.
+
+        Returns
+        -------
+        None.
+
+        """
+        for kwarg in kwargs:
+            if not kwarg in self.simulator_kwargs:
+                raise QisJobArgumentException(
+                    f"Unknown kwarg {kwarg} for Aer Simulator"
+                )
         if "noise_model" in kwargs and "noise_model_backend" in kwargs:
             raise QisJobArgumentException(
                 "noise_model and noise_model_backend are mutually exclusive"
             )
         if "method" in kwargs:
             self.method = kwargs["method"]
+
+    def instance_aer_simulator(self) -> AerSimulator:
+        """
+        Take processed kwargs and use them to instance an AerSimulator
+
+        Returns
+        -------
+        AerSimulator
+            The AerSimulator to process the QisJob
+
+        """
+        the_args = []
+        return AerSimulator(**the_args)
